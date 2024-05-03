4463df16e8e9f3972f1938c56c43a5477c00d4b7
Dbritooo <51900880814@info.local>
2024-05-03 15:04:53 -0300

    feat exercicio 1

---
+++ ex-2.js
@@ -0,0 +1,37 @@
+// Exercício 2: Verificação de dia da semana
+// Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da semana e exibe o nome correspondente a esse dia.
+//  Considere que 1 representa segunda-feira, 2 representa terça-feira e assim por diante.
+
+
+
+
+let username = prompt("digite um numero da semana")
+
+if (username == 1) {
+        console.log("domingo")
+
+}
+if (username == 7) {
+        console.log("sabado")
+
+}
+if (username == 6) {
+        console.log("sexta")
+
+}
+if (username == 5) {
+        console.log("quinta")
+
+}
+if (username == 4) {
+        console.log("quarta")
+
+}
+if (username == 3) {
+        console.log("terça")
+
+}
+if (username == 2) {
+        console.log("segunda")
+
+}
---
+++ exemplo.js
@@ -0,0 +1,34 @@
+//alert("Hello-word")
+
+
+//console.log("essa mensagem aparecerá no console - log") // essa mensagem aparece no console
+
+
+//prompt ("recebe uma entrada la emcima")
+
+
+
+//---    let nome = prompt("colocar log:")
+//    console.log ("colocar ele para log");  --
+//              use o + para adicionar 
+
+
+// let pontos = 100
+
+// // parse int () --> transforma o que esta entre () em numeros inteiros.
+// let bonus = parseInt( prompt("quantos pontos voce deseja somar?") )    
+
+
+// adição -> num1 + num2
+// subtração -> num1 - num2
+// multiplicaçao -> num1 * num2
+//divisão -> num1 / num2
+
+
+
+// console.log(pontos + bonus)
+
+
+// parsefloat () -----> tranform o que esta  com , em um numero
+let notaALUNO = parseFloat(prompt ("digite a nota dos alunos ") )
+console.log (notaALUNO);
