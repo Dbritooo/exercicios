0cfcf10c2dbf9839d1f33b0b05294f6e50f54630
Dbritooo <51900880814@info.local>
2024-05-03 15:07:07 -0300

    feat exercicio-2

--- ex-2.js
+++ ex-2.js
@@ -2,9 +2,6 @@
 // Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da semana e exibe o nome correspondente a esse dia.
 //  Considere que 1 representa segunda-feira, 2 representa terça-feira e assim por diante.
 
-
-
-
 let username = prompt("digite um numero da semana")
 
 if (username == 1) {
