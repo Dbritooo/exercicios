fd5d9de0e8f4a5cec9b27207529de1ca7877b0da
Dbritooo <51900880814@info.local>
2024-05-03 15:17:19 -0300

    feat exercicio-3

---
+++ ex-3.js
@@ -0,0 +1,12 @@
+// Exercício 3: Ímpar ou par
+// Faça um programa que receba um número do usuário e informe se este número é par ou ímpar.
+
+let username = prompt("digite um numero da impar ou par")
+
+var num = 10;
+ 
+if (num % 2 === 0) {
+  console.log('Par');
+} else {
+  console.log('Ímpar');
+}
