601423527b3a2d0e58cf1e373d59ba11f7b34782
Dbritooo <51900880814@info.local>
2024-05-03 17:08:30 -0300

    feat: exercicio 7

--- ex-7.js
+++ ex-7.js
@@ -4,3 +4,21 @@
 // Valor total da compra de R$ 100,01 a R$ 200,00: desconto de 10%.
 // Valor total da compra acima de R$ 200,00: desconto de 20%.
 // Exiba o valor final da compra com o desconto aplicado.
+
+
+const valorTotalCompra = parseFloat(prompt("Digite o valor total da compra: "));
+
+let desconto;
+if (valorTotalCompra <= 100) {
+    desconto = 0;
+} else if (valorTotalCompra <= 200) {
+    desconto = valorTotalCompra * 0.1;
+} else {
+    desconto = valorTotalCompra * 0.2;
+}
+
+const valorFinal = valorTotalCompra - desconto;
+
+console.log("Valor total da compra: R$ " + valorTotalCompra);
+console.log("Desconto aplicado: R$ " + desconto);
+console.log("Valor final com desconto: R$ " + valorFinal);
